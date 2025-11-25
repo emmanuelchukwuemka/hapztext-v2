62e5199c9f407c93171ba19cc23a258e6a42ea72
johnson-oragui <johnson.oragui@gmail.com>
2025-11-25 09:28:25 +0100

    feat: imported repositories into init

---
+++ apps/infrastructure/calls/repository/__init__.py
@@ -0,0 +1,10 @@
+"""
+Init
+"""
+
+from apps.infrastructure.calls.repository.call_record_repo import (
+    CallRecordRepository,
+)
+from apps.infrastructure.calls.repository.discover_redis_repo import (
+    DiscoverRepository,
+)
