b22e0f52507df51a27cfed1564ec9a410db7ae6c
johnson-oragui <johnson.oragui@gmail.com>
2025-11-25 09:30:29 +0100

    feat: added DTOs to init for access

---
+++ apps/infrastructure/calls/dto/__init__.py
@@ -0,0 +1,17 @@
+"""
+Chat DTO INIT
+"""
+
+from apps.infrastructure.calls.dto.webrtc_dto import MediaStateValidatorDto
+from apps.infrastructure.calls.dto.call_dto import (
+    CallBaseDto,
+    UpdateCallRequestDto,
+    FetchCallHistoryQueryDto,
+    FetchCallHistoryResponseDto,
+    NewCallRequestDto,
+    JoinCallRequestDto,
+    EndCallRequestDto,
+    InviteCallRequestDto,
+    GetCallHistoryRequestDto,
+    UpdateCallTitleRequestDto,
+)
